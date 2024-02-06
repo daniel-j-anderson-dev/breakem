331a3fe9183b3aace45cd462e3060f32a847660c
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-02-06 14:47:22 -0500

    Reformatted display_menu

--- src/game.rs
+++ src/game.rs
@@ -62,24 +62,24 @@ impl Game {
         };
     }
     fn display_menu(&mut self, menu: Menu) {
-        let screen_size = screen_size();
+        let screen_origin = screen_size() / 2.0;
 
-        let buffer_space: f32 = (screen_size.y / 20.0).max(21.0);
+        let buffer_space: f32 = (screen_origin.y / 10.0).max(21.0);
 
-        let label_position = Vec2::new(screen_size.x / 2.0, screen_size.y / 4.0 + buffer_space);
-        let play_button_position =
-            Vec2::new(label_position.x, screen_size.y / 4.0 + buffer_space * 2.0);
-        let exit_button_position =
-            Vec2::new(label_position.x, screen_size.y / 4.0 + buffer_space * 3.0);
+        let [label_position, play_button_position, exit_button_position] = [
+            Vec2::new(screen_origin.x, screen_origin.y / 2.0 + buffer_space),
+            Vec2::new(screen_origin.x, screen_origin.y / 2.0 + buffer_space * 2.0),
+            Vec2::new(screen_origin.x, screen_origin.y / 2.0 + buffer_space * 3.0),
+        ];
 
-        let (label_text, play_button_text, exit_button_text) = match menu {
-            Menu::Main => ("Breakem", "start game", "quit"),
-            Menu::Pause => ("Paused", "continue game", "return to main menu"),
-            Menu::GameOver => ("GAME OVER", "continue game", "return to main menu"),
-            Menu::LevelComplete => ("Level Complete", "next level", "return to main menu"),
+        let [label_text, play_button_text, exit_button_text] = match menu {
+            Menu::Main => ["Breakem", "start game", "quit"],
+            Menu::Pause => ["Paused", "continue game", "return to main menu"],
+            Menu::GameOver => ["GAME OVER", "continue game", "return to main menu"],
+            Menu::LevelComplete => ["Level Complete", "next level", "return to main menu"],
         };
 
-        let window = Window::new(0, Vec2::ZERO, screen_size);
+        let window = Window::new(0, Vec2::ZERO, screen_origin);
         window.ui(&mut root_ui(), |ui| {
             ui.label(label_position, label_text);
 
@@ -87,8 +87,8 @@ impl Game {
                 match menu {
                     Menu::Main => self.full_reset(),
                     Menu::GameOver => self.level_reset(),
+                    Menu::LevelComplete => todo!(),
                     Menu::Pause => {}
-                    Menu::LevelComplete => todo!("change level"),
                 };
                 self.state = GameState::Playing
             }
@@ -96,9 +96,7 @@ impl Game {
             if ui.button(exit_button_position, exit_button_text) {
                 self.state = match menu {
                     Menu::Main => GameState::Quit,
-                    Menu::Pause | Menu::GameOver | Menu::LevelComplete => {
-                        GameState::NotPlaying(Menu::Main)
-                    }
+                    _ => GameState::NotPlaying(Menu::Main),
                 }
             }
         });
@@ -233,8 +231,8 @@ fn seed_random_with_current_time() {
 
     srand(
         SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards")
-        .as_secs()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_secs()
     );
 }
